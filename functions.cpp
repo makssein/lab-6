8d873f65ed41aac3cb0d68fe198366927acd3045
makssein <90086818+makssein@users.noreply.github.com>
2021-10-08 23:39:49 +0200

    Create functions.cpp

---
+++ functions.cpp
@@ -0,0 +1,33 @@
+#include "functions.hpp"
+
+#define N 100
+#define M 100
+
+namespace func{
+    /*int MinPrime(int& n, int& m, int matrix[N][M]){
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if (matrix[i][j] >= 2){
+                    for (int d = 2; d < matrix[i][j]; d++){
+                        if(matrix[i][j] % d == 0)
+                            checkPr = false;
+                        if(checkPr){
+                            if(matrix[i][j] < minPrime)
+                                minPrime = matrix[i][j];
+                        }
+                            
+                    }
+                }
+            }
+        }
+    }
+    void Read(int& n, int& m, int mas[N][M]){
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                in >> matrix[i][j];
+            }
+        }
+    }*/
+
+}
+
