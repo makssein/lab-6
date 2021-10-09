67767e6f28764d46b4678a949936158797e7c876
makssein <90086818+makssein@users.noreply.github.com>
2021-10-09 10:34:04 +0200

    Update functions.hpp

--- functions.hpp
+++ functions.hpp
@@ -2,6 +2,11 @@
 
 #define N 100
 #define M 100
-namespace func{
-    void Read(int& n, int& m, int mas[N][M]);
+namespace func {
+	void Read(int& n, int& m, int matrix[N][M]);
+	bool Checking(int& n, int& m, int matrix[N][M]);
+	bool isPrime(int x);
+	int maxPrimeFunc(int& n, int& m, bool& checkPr, int matrix[N][M]);
+	void ReplaceElements(int& n, int& m, int matrix[N][M], int& maxPrime);
+	void write(int& n, int& m, int matrix[N][M]);
 }
